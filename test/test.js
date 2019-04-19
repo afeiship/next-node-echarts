2f192d30d2d9b18843d2499952f849ee4d793d18
afeiship <1290657123@qq.com>
2019-04-19 10:06:45 +0800

    add: test png

--- test/test.js
+++ test/test.js
@@ -6,7 +6,7 @@ var data_bar = require('./data/bar');
 var data_bubble = require('./data/bubble');
 var data_duidie = require('./data/duidie');
 
-var items = [data_area, data_pie, data_bar, data_bubble, data_duidie];
+var items = [data_area /*data_pie, data_bar, data_bubble, data_duidie */];
 
 items.forEach(function(item, index) {
   var echarts = new NxNodeEcharts({
@@ -16,8 +16,10 @@ items.forEach(function(item, index) {
   });
 
   var b64 = echarts.getDataURL();
+  var filebuf = echarts.toBuffer();
   var filename = './test/outputs/' + index + '.html';
   fs.writeFileSync(filename, `<img src="${b64}" width="1000" height="500" />`);
+  fs.writeFileSync('./test/outputs/0.png', filebuf);
   echarts.dispose();
-  console.log(`File created at: http://0.0.0.0:3000/outputs/${index}.html`);
+  console.log(`File created at: http://0.0.0.0:3000/outputs/${index}.png`);
 });
